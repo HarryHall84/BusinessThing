ae2aed12fd3a852df3d39deb9e439b70405a0093
Carl Show <carl.show@yahoo.com>
2022-11-15 14:35:05 -0600

    Work on the alert controller

--- BusinessThing/ViewController.swift
+++ BusinessThing/ViewController.swift
@@ -1,19 +1,44 @@
-//
-//  ViewController.swift
-//  BusinessThing
-//
-//  Created by HARRISON HALL on 11/14/22.
-//
-
+/// Created by Harrison Hall & Carl Show on 11/14/22.
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var misAssignments: UITabBarItem!
+    var isLogged = false
+    @IBOutlet weak var calHiearchy: UITabBarItem!
+    @IBOutlet weak var UpAssign: UITabBarItem!
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func Login(_ sender: Any)
+    {
+        var name = ""
+        var pass = ""
+        let err = UIAlertController(title: "Enter the following information to log in...", message: "", preferredStyle: .alert)
+        err.addTextField{(textField) in
+            textField.placeholder = "Name"}
+        err.addTextField{(textField) in
+            textField.placeholder = "Password"}
+        err.addAction(UIAlertAction(title: "Submit", style: .default, handler: { alert -> Void in
+            name = err.textFields![0].text ?? "Unknown"
+            pass = err.textFields![1].text ?? "Empty"
+            if(name == "debugDan" && pass == "123")
+            {
+                self.isLogged = true
+            }
+        }))
+        err.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(err, animated: true, completion: nil)
+    }
+    func updateAll()
+    {
+        if(isLogged)
+        {
+            loginButton.isHidden = true
+            UpAssign.isEnabled = true
+            calHiearchy.isEnabled = true
+            misAssignments.isEnabled = true
+        }
+    }
 }
-
