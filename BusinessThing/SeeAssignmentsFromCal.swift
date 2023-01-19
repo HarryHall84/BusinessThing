f0a4bc8f712f2135d6408c00baaf5e11ed417e53
Carl Show <carl.show@yahoo.com>
2023-01-19 10:02:37 -0600

    Sometimes, I want to defenestrate myself.

--- BusinessThing/SeeAssignmentsFromCal.swift
+++ BusinessThing/SeeAssignmentsFromCal.swift
@@ -5,8 +5,30 @@
 //  Created by CARL SHOW on 1/18/23.
 //
 
-import Foundation
 import UIKit
-class SeeAssignmentsFromCal: UIViewController {
+
+class CalendarSeeAssignmentCustomCell: UITableViewCell {
+    
+    
+    
+}
+
+
+class SeeAssignmentsFromCal: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+ 
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
     
 }
